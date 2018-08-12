6e7c49ffd1298f723d89d1239d73c41997252130
aboud20-meet <aboud20@meet.mit.edu>
2018-08-12 16:18:47 +0300

    fixed bugs

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
@@ -9,24 +9,27 @@ Base = declarative_base()
 Base = declarative_base()
 # Example code:
 class Volunteer(Base):
-    __tablename__ = "students"
+    __tablename__ = "volunteers"
     id = Column(Integer, primary_key = True)
     name = Column(String)
     age = Column(Integer)
     location = Column(String)
     phone = Column(Integer)
     info = Column(String)
+    elders = relationship("Elder", back_populates="volunteer")
 
     def __repr__(self):
         return ("Volunteer name: {}, Volunteer age: {}, Volunteer location: {}, Volunteer phone: {}, Volunteer info: {}".format(self.name, self.age, self.location, self.phone, self.info))
 
 class Elder(Base):
-    __tablename__ = "students"
+    __tablename__ = "elders"
     id = Column(Integer, primary_key = True)
     name = Column(String)
     location = Column(String)
     phone = Column(Integer)
     info = Column(String)
+    volunteer_id = Column(Integer, ForeignKey(Volunteer.id))
+    volunteer = relationship('Volunteer')
 
     def __repr__(self):
         return ("Elder name: {}, Elder location: {}, Elder phone: {}, Elder info: {}".format(self.name, self.location, self.phone, self.info))
