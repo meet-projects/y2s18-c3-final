3027fb87826e9d8c7e47245e8c2511d4c31d3f32
Simon Deeb <simon19-meet@meet.mit.edu>
2018-08-14 05:37:09 -0700

    fixing login

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app.secret_key= b'gh57658tsxyh'
 # App routing code here
 @app.route('/')
 def home():
-    if 'username' not in session:
-        return render_template('log_in.html')
+    '''if 'username' not in session:
+        return render_template('log_in.html')'''
     return render_template('home.html')
 def logout():
     session.RemoveAll()
@@ -36,7 +36,14 @@ def login():
         eld=query_by_elder_name(name)
         if eld!=None:
             if password==eld.password:
-                session.add(eld)
+                session['username']=eld.name
+                session['password']=eld.password
+                session['age']=eld.age
+                session['location']=eld.location
+                session['phone']=eld.phone
+                session['id']=eld.id
+                session['info']=eld.info
+                session['vol_id']=eld.volunteer_id
                 a="<h1>Welcome</h1>"
                 return render_template('home.html',a=a)
             else:
